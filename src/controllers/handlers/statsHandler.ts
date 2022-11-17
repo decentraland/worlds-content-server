c006331a2e8013bf47046f54c591e2682072f6f7
Mariano Goldman <mariano.goldman@decentraland.org>
2022-11-17 17:32:04 -0300

    use storage methods to fetch keys

--- src/controllers/handlers/statsHandler.ts
+++ src/controllers/handlers/statsHandler.ts
@@ -1,28 +1,14 @@
 import { HandlerContextWithPath } from "../../types"
-import { S3 } from "aws-sdk";
 
-export async function statsHandler({ components: { config } }: Pick<HandlerContextWithPath<"config", "/stats">, "components" | "params" | "url">) {
-  const region = await config.requireString("AWS_REGION");
-  const s3 = new S3({ region })
-  const params: S3.Types.ListObjectsV2Request = {
-    Bucket: 'orco-backups',
-    ContinuationToken: undefined
-  };
-
-  const objects = []
-  let fetched: S3.ListObjectsV2Output
-  do {
-    fetched = await s3.listObjectsV2(params).promise()
-    objects.push(...fetched.Contents!)
-    params.ContinuationToken = fetched.NextContinuationToken
-  } while (fetched.IsTruncated)
-
-  let keys = objects
-      .map(o => o.Key!)
-      .filter(o => o.endsWith('.dcl.eth'));
+export async function statsHandler({ components: { storage } }: Pick<HandlerContextWithPath<"storage", "/stats">, "components" | "params" | "url">) {
+  const filtered = []
+  for await (const key of await storage.allFileIds("name-")) {
+    if (key.endsWith('.dcl.eth'))
+      filtered.push(key.substring(5)) // remove "name-" prefix
+  }
 
   return {
     status: 200,
-    body: keys,
+    body: filtered,
   }
 }
