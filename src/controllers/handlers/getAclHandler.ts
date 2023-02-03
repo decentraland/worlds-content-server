85f97f6defcb254143ce2f81fc0030c1063c4815
Mariano Goldman <mariano.goldman@decentraland.org>
2023-02-03 12:38:32 -0300

    feat: get /acl endpoint

---
+++ src/controllers/handlers/getAclHandler.ts
@@ -0,0 +1,22 @@
+import { IHttpServerComponent } from '@well-known-components/interfaces'
+import { HandlerContextWithPath } from '../../types'
+
+export async function getAclHandler(
+  ctx: HandlerContextWithPath<
+    'config' | 'logs' | 'namePermissionChecker' | 'metrics' | 'storage' | 'sns' | 'validator',
+    '/acl'
+  >
+): Promise<IHttpServerComponent.IResponse> {
+  const logger = ctx.components.logs.getLogger('deploy')
+  try {
+    return {
+      status: 200,
+      body: {
+        acl: {}
+      }
+    }
+  } catch (err: any) {
+    logger.error(err)
+    throw err
+  }
+}
