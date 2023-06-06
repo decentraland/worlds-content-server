0e962cd37d96d37e6d6e5858a2d0ecf87a8f3b58
Mariano Goldman <mariano.goldman@decentraland.org>
2023-06-06 11:04:34 -0300

    data for indexing

--- test/integration/mariano.spec.ts
+++ test/integration/mariano.spec.ts
@@ -1,67 +1,34 @@
 import { createFetchComponent } from '../../src/adapters/fetch'
-import { IFetchComponent } from '@well-known-components/http-server'
-import PQueue from 'p-queue'
-
-async function fetchForWorld(baseUrl: string, name: string, fetcher: IFetchComponent) {
-  const aboutRes = await fetcher.fetch(`${baseUrl}/world/${name}/about`)
-  const about = await aboutRes.json()
-
-  const entities = {}
-  for (const urn of about.configurations.scenesUrn) {
-    const ipfsRegex = /^urn:decentraland:entity:([a-zA-Z0-9]+)\?.*$/
-    const match = urn.match(ipfsRegex)
-    if (!match) {
-      continue
-    }
-
-    const entityId = match[1]
-    try {
-      const sceneJsonRes = await fetcher.fetch(`${baseUrl}/contents/${entityId}`)
-      const sceneJson = await sceneJsonRes.json()
-      if (!sceneJson) {
-        continue
-      }
-      entities[entityId] = {
-        parcels: sceneJson.pointers.length,
-        sdk: sceneJson.metadata?.runtimeVersion
-      }
-    } catch (e) {
-      console.log('ERROR', e.message)
-    }
-    return entities
-  }
-}
+import * as fs from 'fs'
 
 describe('All data from worlds', function () {
-  const baseUrl = 'http://localhost:3000'
-  const authorization = 'Bearer mariano'
+  // const baseUrl = 'http://localhost:3000'
+  // const baseUrl = 'https://worlds-content-server.decentraland.zone'
+  const baseUrl = 'https://worlds-content-server.decentraland.org'
 
   it(
     'returns list of worlds together with number of parcels for each',
     async () => {
       const fetcher = await createFetchComponent()
-      const statusRes = await fetcher.fetch(`${baseUrl}/status`, {
-        headers: {
-          Authorization: authorization
+      const indexResponse = await fetcher.fetch(`${baseUrl}/index`)
+      const index = await indexResponse.json()
+
+      const worldNames = index.data.map((world: any) => world.name)
+      console.log(JSON.stringify(worldNames, undefined, 2))
+
+      const statsResponse = await fetcher.fetch(
+        `${baseUrl.replace('worlds-content-server', 'dcl-name-stats')}/should-index`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            dclNames: worldNames
+          })
         }
-      })
-      const status = await statusRes.json()
+      )
+      const stats = await statsResponse.json()
+      console.log(JSON.stringify(stats, undefined, 2))
 
-      const queue = new PQueue({ concurrency: 10 })
-      const worldsWithParcels = {}
-      for (const [index, name] of status.content.details.entries()) {
-        void queue.add(async () => {
-          try {
-            console.log(`Fetching data about world #${index} ${name}`)
-            worldsWithParcels[name] = await fetchForWorld(baseUrl, name, fetcher)
-          } catch (e) {
-            console.log('ERROR', e)
-          }
-        })
-      }
-
-      await queue.onEmpty()
-      console.log(JSON.stringify(worldsWithParcels, undefined, 2))
+      fs.writeFileSync('contents/data-for-indexing.json', JSON.stringify(stats, undefined, 2))
     },
     10 * 60 * 1000 // 10 minutes
   )
