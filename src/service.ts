53a1e12fb6275f9358acbd4d0e018b677ca5dd30
Mariano Goldman <mariano.goldman@decentraland.org>
2023-06-23 19:58:44 -0300

    migration to fix incorrectly stored ACLs

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,8 @@
 import { Lifecycle } from '@well-known-components/interfaces'
 import { setupRouter } from './controllers/routes'
 import { AppComponents, GlobalContext, TestComponents } from './types'
+import { bufferToStream, streamToBuffer } from '@dcl/catalyst-storage/dist/content-item'
+import { stringToUtf8Bytes } from 'eth-connect'
 
 // this function wires the business logic (adapters & controllers) with the components (ports)
 export async function main(program: Lifecycle.EntryPointParameters<AppComponents | TestComponents>) {
@@ -21,13 +23,33 @@ export async function main(program: Lifecycle.EntryPointParameters<AppComponents
   // start ports: db, listeners, synchronizations, etc
   await startComponents()
 
-  // Migrate old name pointers to new one
+  async function readFile(key: string): Promise<object | undefined> {
+    const content = await components.storage.retrieve(key)
+    if (!content) {
+      return undefined
+    }
+    return JSON.parse((await streamToBuffer(await content.asStream())).toString())
+  }
+
+  async function writeFile(key: string, content: object) {
+    console.log(`Writing "${key}"`, JSON.stringify(content))
+    await components.storage.storeStream(key, bufferToStream(stringToUtf8Bytes(JSON.stringify(content))))
+  }
+
+  // Fix incorrectly stored ACLs
   for await (const key of await components.storage.allFileIds('name-')) {
     if (key.startsWith('name-') && key.endsWith('.dcl.eth')) {
-      console.log(`Renaming "${key}" to "name-${key}"`)
-      // const fileContent = await components.storage.retrieve(key)
-      // await components.storage.storeStream(`name-${key}`, await fileContent?.asStream()!)
-      // await components.storage.delete([key])
+      if (key.toLowerCase() !== key) {
+        console.log(`Found "${key}" that needs to be fixed`)
+        const existing = await readFile(key)
+        const toBeModified = (await readFile(key.toLowerCase())) || {}
+        console.log({ existing, toBeModified })
+        await writeFile(key.toLowerCase(), {
+          ...existing,
+          ...toBeModified
+        })
+        await components.storage.delete([key])
+      }
     }
   }
 }
