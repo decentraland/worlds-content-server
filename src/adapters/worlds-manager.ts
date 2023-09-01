3cf9a0051c0d7d984179fc7cb35a1fe573bc0c77
Mariano Goldman <mariano.goldman@decentraland.org>
2023-09-01 20:37:14 -0300

    wip

--- src/adapters/worlds-manager.ts
+++ src/adapters/worlds-manager.ts
@@ -1,10 +1,10 @@
 import { AppComponents, IWorldsManager, WorldMetadata } from '../types'
-import LRU from 'lru-cache'
 import { bufferToStream, streamToBuffer } from '@dcl/catalyst-storage'
 import { AuthChain, Entity } from '@dcl/schemas'
 import { stringToUtf8Bytes } from 'eth-connect'
 import SQL from 'sql-template-strings'
 import { extractWorldRuntimeMetadata } from '../logic/world-runtime-metadata-utils'
+import { deepEqual } from '../logic/deep-equal'
 
 type WorldRecord = {
   name: string
@@ -23,70 +23,35 @@ export async function createWorldsManagerComponent({
   pg,
   storage
 }: Pick<AppComponents, 'logs' | 'pg' | 'storage'>): Promise<IWorldsManager> {
-  const logger = logs.getLogger('worlds-manager')
-  const WORLDS_KEY = 'worlds'
+  // const logger = logs.getLogger('worlds-manager')
+  // const WORLDS_KEY = 'worlds'
+  //
+  // const cache = new LRU<string, string[]>({
+  //   max: 1,
+  //   ttl: 10 * 60 * 1000, // cache for 10 minutes
+  //   fetchMethod: async (_, staleValue): Promise<string[] | undefined> => {
+  //     try {
+  //       const worlds = []
+  //       for await (const key of storage.allFileIds('name-')) {
+  //         worlds.push(key.substring(5)) // remove "name-" prefix
+  //       }
+  //       return worlds
+  //     } catch (_: any) {
+  //       logger.warn(`Error retrieving worlds from storage: ${_.message}`)
+  //       return staleValue
+  //     }
+  //   }
+  // })
 
-  const cache = new LRU<string, string[]>({
-    max: 1,
-    ttl: 10 * 60 * 1000, // cache for 10 minutes
-    fetchMethod: async (_, staleValue): Promise<string[] | undefined> => {
-      // try {
-      //   const result = await pg.query('SELECT name FROM worlds ORDER BY name')
-      //   return result.rows.map((row) => row.name)
-      // } catch (_: any) {
-      //   logger.warn(`Error retrieving worlds from db: ${_.message}`)
-      //   return staleValue
-      // }
-      try {
-        const worlds = []
-        for await (const key of storage.allFileIds('name-')) {
-          worlds.push(key.substring(5)) // remove "name-" prefix
-        }
-        return worlds
-      } catch (_: any) {
-        logger.warn(`Error retrieving worlds from storage: ${_.message}`)
-        return staleValue
-      }
-    }
-  })
-
-  const worldsCache = new LRU<string, WorldMetadata>({
-    max: 100,
-    ttl: 2 * 1000, // cache for 2 seconds (should be enough for multiple accesses during the same request)
-    fetchMethod: async (worldName, staleValue): Promise<WorldMetadata | undefined> => {
-      console.log('worldName', worldName)
-      const result = await pg.query<WorldRecord>(SQL`SELECT * FROM worlds WHERE name = ${worldName.toLowerCase()}`)
-      if (result.rowCount === 0) {
-        return undefined
-      }
-
-      const row = result.rows[0]
-      const runtimeMetadata = extractWorldRuntimeMetadata(worldName, { ...row.metadata, id: row.entity_id })
-
-      const fromDb: WorldMetadata = JSON.parse(
-        JSON.stringify({
-          entityId: row.entity_id,
-          runtimeMetadata,
-          acl: row.acl || undefined
-        })
-      )
-      console.log('fromDb', fromDb)
-
-      const content = await storage.retrieve(`name-${worldName.toLowerCase()}`)
-      if (!content) {
-        return staleValue
-      }
-      const fromStorage = JSON.parse((await streamToBuffer(await content.asStream())).toString())
-
-      if (fromDb !== fromStorage) {
-        console.log('fromStorage', fromStorage)
-      }
-      return fromStorage
-    }
-  })
-
+  // const worldsCache = new LRU<string, WorldMetadata>({
+  //   max: 100,
+  //   ttl: 2 * 1000, // cache for 2 seconds (should be enough for multiple accesses during the same request)
+  //   fetchMethod: async (worldName, staleValue): Promise<WorldMetadata | undefined> => {}
+  // })
+  //
   async function getDeployedWorldsNames(): Promise<string[]> {
-    return (await cache.fetch(WORLDS_KEY))!
+    const result = await pg.query('SELECT name FROM worlds ORDER BY name')
+    return result.rows.map((row) => row.name)
   }
 
   async function getEntityForWorld(worldName: string): Promise<Entity | undefined> {
@@ -111,7 +76,33 @@ export async function createWorldsManagerComponent({
   }
 
   async function getMetadataForWorld(worldName: string): Promise<WorldMetadata | undefined> {
-    return await worldsCache.fetch(worldName)
+    // console.log('worldName', worldName)
+    const result = await pg.query<WorldRecord>(SQL`SELECT * FROM worlds WHERE name = ${worldName.toLowerCase()}`)
+    if (result.rowCount === 0) {
+      return undefined
+    }
+
+    const row = result.rows[0]
+    const tempWorldMetadata: Partial<WorldMetadata> = {}
+    if (row.entity_id) {
+      tempWorldMetadata.entityId = row.entity_id
+      tempWorldMetadata.runtimeMetadata = extractWorldRuntimeMetadata(worldName, { ...row.metadata, id: row.entity_id })
+    }
+    if (row.acl) {
+      tempWorldMetadata.acl = row.acl
+    }
+    const fromDb: WorldMetadata = JSON.parse(JSON.stringify(tempWorldMetadata))
+
+    const content = await storage.retrieve(`name-${worldName.toLowerCase()}`)
+    if (!content) {
+      return undefined
+    }
+    const fromStorage = JSON.parse((await streamToBuffer(await content.asStream())).toString())
+
+    if (!deepEqual(fromDb, fromStorage)) {
+      console.warn('fromDb', fromDb, 'fromStorage', fromStorage)
+    }
+    return fromStorage
   }
 
   async function storeWorldMetadata(worldName: string, worldMetadata: Partial<WorldMetadata>): Promise<void> {
@@ -125,7 +116,7 @@ export async function createWorldsManagerComponent({
       bufferToStream(stringToUtf8Bytes(JSON.stringify(metadata)))
     )
 
-    worldsCache.delete(worldName)
+    // worldsCache.delete(worldName)
   }
 
   async function deployScene(worldName: string, scene: Entity): Promise<void> {
@@ -133,9 +124,44 @@ export async function createWorldsManagerComponent({
       entityId: scene.id,
       runtimeMetadata: extractWorldRuntimeMetadata(worldName, scene)
     })
+
+    const content = await storage.retrieve(`${scene.id}.auth`)
+    const deploymentAuthChainString = content ? (await streamToBuffer(await content!.asStream())).toString() : '{}'
+    const deploymentAuthChain = JSON.parse(deploymentAuthChainString)
+
+    const deployer = deploymentAuthChain[0].payload.toLowerCase()
+    // console.log({ worldName, entityId: scene.id, deployer, deploymentAuthChain })
+
+    const sql = SQL`
+              INSERT INTO worlds (name, entity_id, deployer, deployment_auth_chain, metadata, created_at, updated_at)
+              VALUES (${worldName}, ${scene.id}, ${deployer}, ${deploymentAuthChainString}::json,
+                      ${scene.metadata}::json,
+                      ${new Date()}, ${new Date()})
+              ON CONFLICT (name) 
+                  DO UPDATE SET entity_id = ${scene.id}, 
+                                deployer = ${deployer},
+                                metadata = ${scene.metadata}::json,
+                                deployment_auth_chain = ${deploymentAuthChainString}::json,
+                                updated_at = ${new Date()}
+                `
+    // console.log('sql', sql.sql)
+    // console.log('query', sql.query)
+    // console.log('values', sql.values)
+    await pg.query(sql).catch((error) => {
+      console.log('error', error)
+      throw error
+    })
   }
 
   async function storeAcl(worldName: string, acl: AuthChain): Promise<void> {
+    const sql = SQL`
+              INSERT INTO worlds (name, acl, created_at, updated_at)
+              VALUES (${worldName}, ${JSON.stringify(acl)}::json, ${new Date()}, ${new Date()})
+              ON CONFLICT (name) 
+                  DO UPDATE SET acl = ${JSON.stringify(acl)}::json, updated_at = ${new Date()}
+                `
+    await pg.query(sql)
+
     await storeWorldMetadata(worldName, { acl })
   }
 
--- test/components.ts
+++ test/components.ts
@@ -9,7 +9,6 @@ import { initComponents as originalInitComponents } from '../src/components'
 import { createMockMarketplaceSubGraph } from './mocks/marketplace-subgraph-mock'
 import { createMockNamePermissionChecker } from './mocks/dcl-name-checker-mock'
 import { createMockLimitsManagerComponent } from './mocks/limits-manager-mock'
-import { createWorldsManagerMockComponent } from './mocks/worlds-manager-mock'
 import { createMockStatusComponent } from './mocks/status-mock'
 import { createInMemoryStorage } from '@dcl/catalyst-storage'
 import { createMockCommsAdapterComponent } from './mocks/comms-adapter-mock'
@@ -22,6 +21,7 @@ import { metricDeclarations } from '../src/metrics'
 import { createEntityDeployer } from '../src/adapters/entity-deployer'
 import { createMockNameDenyListChecker } from './mocks/name-deny-list-checker-mock'
 import { createWorldCreator } from './mocks/world-creator'
+import { createWorldsManagerComponent } from '../src/adapters/worlds-manager'
 
 /**
  * Behaves like Jest "describe" function, used to describe a test for a
@@ -38,7 +38,7 @@ export const test = createRunner<TestComponents>({
 async function initComponents(): Promise<TestComponents> {
   const components = await originalInitComponents()
 
-  const { config, logs } = components
+  const { config, logs, pg } = components
 
   const metrics = createTestMetricsComponent(metricDeclarations)
 
@@ -65,7 +65,7 @@ async function initComponents(): Promise<TestComponents> {
 
   const commsAdapter = createMockCommsAdapterComponent()
 
-  const worldsManager = await createWorldsManagerMockComponent({ storage })
+  const worldsManager = await createWorldsManagerComponent({ logs, pg, storage })
   const worldsIndexer = await createWorldsIndexerComponent({
     logs,
     nameDenyListChecker,
