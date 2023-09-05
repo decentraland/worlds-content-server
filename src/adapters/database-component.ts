2995b0a4db3dafff5af0771cb07a323cd88ae863
Mariano Goldman <mariano.goldman@decentraland.org>
2023-09-05 16:37:43 -0300

    better error if no db configured

--- src/adapters/database-component.ts
+++ src/adapters/database-component.ts
@@ -2,12 +2,17 @@ import { createPgComponent, IPgComponent } from '@well-known-components/pg-compo
 import path from 'path'
 
 export async function createDatabaseComponent(components: createPgComponent.NeededComponents): Promise<IPgComponent> {
+  const databaseUrl = await components.config.getString('PG_COMPONENT_PSQL_CONNECTION_STRING')
+  if (!databaseUrl) {
+    throw new Error('Env var PG_COMPONENT_PSQL_CONNECTION_STRING is required. Set it up to point to a database.')
+  }
+
   return await createPgComponent(components, {
     migration: {
       dir: path.resolve(__dirname, '../pgmigrations'),
       migrationsTable: 'migrations',
       direction: 'up',
-      databaseUrl: await components.config.requireString('PG_COMPONENT_PSQL_CONNECTION_STRING'),
+      databaseUrl,
       ignorePattern: '.*\\.map' // avoid sourcemaps
     }
   })
