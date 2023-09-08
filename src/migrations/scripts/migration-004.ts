9be936f9d77a3abb95b1ea184700a6b9b37e68dc
Mariano Goldman <mariano.goldman@decentraland.org>
2023-09-08 08:21:49 -0300

    ACL to permission migration

--- src/migrations/scripts/migration-004.ts
+++ src/migrations/scripts/migration-004.ts
@@ -1,31 +1,19 @@
 import { MigratorComponents } from '../../types'
-import { deepEqual, readFile, writeFile } from '../utils'
 import { defaultPermissions } from '../../logic/permissions-checker'
 
 export default {
-  run: async (components: Pick<MigratorComponents, 'logs' | 'storage'>) => {
+  run: async (components: Pick<MigratorComponents, 'logs' | 'database' | 'worldsManager'>) => {
     const logger = components.logs.getLogger('migration-004')
     logger.info('running migration 004 - acl to permissions')
 
-    for await (const key of components.storage.allFileIds('name-')) {
-      const existing = await readFile(components.storage, key)
-      if (existing) {
-        const worldName = key.replace('name-', '')
-        const permissions = existing.permissions || defaultPermissions()
-        if (existing.acl) {
-          permissions.deployment.wallets = JSON.parse(existing.acl.slice(-1).pop()!.payload).allowed
-          delete existing.acl
-        }
-
-        const migrated = {
-          ...existing,
-          permissions
-        }
-        if (!deepEqual(existing, migrated)) {
-          logger.info(`World "${worldName}" needs to be fixed: ${JSON.stringify(permissions)}`)
-          logger.info(`Writing "${key}" : ${JSON.stringify(migrated)}`)
-          await writeFile(components.storage, key, migrated)
-        }
+    const worlds = await components.database.query('SELECT name, acl FROM worlds ORDER BY name')
+    for (const world of worlds.rows) {
+      const worldName = world.name
+      const permissions = defaultPermissions()
+      if (world.acl) {
+        permissions.deployment.wallets = JSON.parse(world.acl.slice(-1).pop()!.payload).allowed
+        logger.info(`Migrating ACL for "${worldName}" to Permissions: ${JSON.stringify(permissions)}`)
+        await components.worldsManager.storePermissions(worldName, permissions)
       }
     }
   }
