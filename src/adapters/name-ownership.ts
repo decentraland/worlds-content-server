8de76c4f004faecaff328cd76c030ba763dd4915
Mariano Goldman <mariano.goldman@decentraland.org>
2023-09-19 09:43:59 -0300

    feat: check ens owner on-chain
     Conflicts:
    	package.json

--- src/adapters/name-ownership.ts
+++ src/adapters/name-ownership.ts
@@ -4,6 +4,9 @@ import { ContractFactory, RequestManager } from 'eth-connect'
 import { l1Contracts, L1Network, registrarAbi } from '@dcl/catalyst-contracts'
 import LRU from 'lru-cache'
 import { createSubgraphComponent } from '@well-known-components/thegraph-component'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import namehash from '@ensdomains/eth-ens-namehash'
 
 type NamesResponse = {
   nfts: { name: string; owner: { id: string } }[]
@@ -55,7 +58,7 @@ export async function createDummyNameOwnership(): Promise<INameOwnership> {
 }
 
 export async function createEnsNameOwnership(
-  components: Pick<AppComponents, 'config' | 'fetch' | 'logs' | 'metrics'>
+  components: Pick<AppComponents, 'config' | 'fetch' | 'logs' | 'metrics' | 'ethereumProvider'>
 ): Promise<INameOwnership> {
   const logger = components.logs.getLogger('ens-name-ownership')
   logger.info('Using ENS NameOwnership')
@@ -65,8 +68,23 @@ export async function createEnsNameOwnership(
     return await createDummyNameOwnership()
   }
 
+  const ethNetwork = (await components.config.requireString('ETH_NETWORK')) as L1Network
+  const contracts = l1Contracts[ethNetwork]
+  if (!contracts) {
+    throw new Error(`Invalid ETH_NETWORK: ${ethNetwork}`)
+  }
+
+  // Same address in all networks
+  const ensRegistryContractAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+
+  const requestManager = new RequestManager(components.ethereumProvider)
+  const factory = new ContractFactory(requestManager, ensAbi)
+  const ensRegistryContract = (await factory.at(ensRegistryContractAddress)) as any
+
   const ensSubGraph = await createSubgraphComponent(components, ensSubgraphUrl)
   async function findOwner(ensName: string): Promise<EthAddress | undefined> {
+    const ownerOnChain = await ensRegistryContract.owner(namehash.hash(ensName))
+
     const result = await ensSubGraph.query<NamesResponse>(
       `query FetchOwnerForEnsName($ensName: String) {
       nfts: domains(where: {name_in: [$ensName]}) {
@@ -81,6 +99,10 @@ export async function createEnsNameOwnership(
 
     const owners = result.nfts.map(({ owner }) => owner.id.toLowerCase())
     const owner = owners.length > 0 ? owners[0] : undefined
+
+    if (ownerOnChain !== owner) {
+      logger.warn(`Owner of ENS name '${ensName}' is ${owner}, but on-chain it is ${ownerOnChain}`)
+    }
 
     logger.debug(`Owner of ENS name '${ensName}' is ${owner}`)
 
@@ -181,3 +203,206 @@ export async function createCachingNameOwnership(nameOwnership: INameOwnership):
     findOwner
   }
 }
+
+const ensAbi = [
+  {
+    inputs: [{ internalType: 'contract ENS', name: '_old', type: 'address' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'address', name: 'owner', type: 'address' },
+      { indexed: true, internalType: 'address', name: 'operator', type: 'address' },
+      { indexed: false, internalType: 'bool', name: 'approved', type: 'bool' }
+    ],
+    name: 'ApprovalForAll',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { indexed: true, internalType: 'bytes32', name: 'label', type: 'bytes32' },
+      { indexed: false, internalType: 'address', name: 'owner', type: 'address' }
+    ],
+    name: 'NewOwner',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { indexed: false, internalType: 'address', name: 'resolver', type: 'address' }
+    ],
+    name: 'NewResolver',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { indexed: false, internalType: 'uint64', name: 'ttl', type: 'uint64' }
+    ],
+    name: 'NewTTL',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { indexed: false, internalType: 'address', name: 'owner', type: 'address' }
+    ],
+    name: 'Transfer',
+    type: 'event'
+  },
+  {
+    constant: true,
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'operator', type: 'address' }
+    ],
+    name: 'isApprovedForAll',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'old',
+    outputs: [{ internalType: 'contract ENS', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: 'bytes32', name: 'node', type: 'bytes32' }],
+    name: 'owner',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: 'bytes32', name: 'node', type: 'bytes32' }],
+    name: 'recordExists',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: 'bytes32', name: 'node', type: 'bytes32' }],
+    name: 'resolver',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'address', name: 'operator', type: 'address' },
+      { internalType: 'bool', name: 'approved', type: 'bool' }
+    ],
+    name: 'setApprovalForAll',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'address', name: 'owner', type: 'address' }
+    ],
+    name: 'setOwner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'resolver', type: 'address' },
+      { internalType: 'uint64', name: 'ttl', type: 'uint64' }
+    ],
+    name: 'setRecord',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'address', name: 'resolver', type: 'address' }
+    ],
+    name: 'setResolver',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'bytes32', name: 'label', type: 'bytes32' },
+      { internalType: 'address', name: 'owner', type: 'address' }
+    ],
+    name: 'setSubnodeOwner',
+    outputs: [{ internalType: 'bytes32', name: '', type: 'bytes32' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'bytes32', name: 'label', type: 'bytes32' },
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'resolver', type: 'address' },
+      { internalType: 'uint64', name: 'ttl', type: 'uint64' }
+    ],
+    name: 'setSubnodeRecord',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: 'bytes32', name: 'node', type: 'bytes32' },
+      { internalType: 'uint64', name: 'ttl', type: 'uint64' }
+    ],
+    name: 'setTTL',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: 'bytes32', name: 'node', type: 'bytes32' }],
+    name: 'ttl',
+    outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
