61f6294564612afcbdfa06bcb2c6eaaeee1dd9b9
Mariano Goldman <mariano.goldman@decentraland.org>
2023-10-04 17:11:25 -0300

    logging

--- src/controllers/handlers/undeploy-entity-handler.ts
+++ src/controllers/handlers/undeploy-entity-handler.ts
@@ -4,22 +4,24 @@ import { DecentralandSignatureContext } from '@dcl/platform-crypto-middleware'
 
 export async function undeployEntity({
   params,
-  components: { namePermissionChecker, worldsManager },
+  components: { logs, namePermissionChecker, worldsManager },
   verification
-}: HandlerContextWithPath<'namePermissionChecker' | 'worldsManager', '/entities/:world_name'> &
+}: HandlerContextWithPath<'logs' | 'namePermissionChecker' | 'worldsManager', '/entities/:world_name'> &
   DecentralandSignatureContext<any>): Promise<IHttpServerComponent.IResponse> {
+  const logger = logs.getLogger('worlds-manager')
+
   const identity = verification!.auth
 
   const isOwner = await namePermissionChecker.checkPermission(identity, params.world_name)
   if (!isOwner) {
     const checkerForWorld = await worldsManager.permissionCheckerForWorld(params.world_name)
     const hasPermission = await checkerForWorld.checkPermission('deployment', verification!.auth!)
-    console.log('hasPermission', verification!.auth!, hasPermission)
     if (!hasPermission) {
       throw new InvalidRequestError('Invalid request. You have no permission to undeploy the scene.')
     }
   }
 
+  logger.info(`Un-deploying world ${params.world_name}`)
   await worldsManager.undeploy(params.world_name)
 
   return {
