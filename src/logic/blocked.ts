7a6201fc2c4c094971556eed196c0ad9c2f700a7
Mariano Goldman <mariano.goldman@decentraland.org>
2023-10-10 09:14:00 -0300

    remove log

--- src/logic/blocked.ts
+++ src/logic/blocked.ts
@@ -4,9 +4,8 @@ const TWO_DAYS = 2 * 24 * 60 * 60 * 1000
 
 export function assertNotBlockedOrWithinInGracePeriod(worldMetadata: WorldMetadata) {
   if (worldMetadata.blockedSince) {
-    const since = new Date(worldMetadata.blockedSince)
     const now = new Date()
-    if (now.getTime() - since.getTime() > TWO_DAYS) {
+    if (now.getTime() - worldMetadata.blockedSince.getTime() > TWO_DAYS) {
       throw new AccessDeniedError(
         `World "${worldMetadata.runtimeMetadata.name}" has been blocked since ${worldMetadata.blockedSince} as it exceeded its allowed storage space.`
       )
