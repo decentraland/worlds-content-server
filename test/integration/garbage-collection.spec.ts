e1faa33a3ae4d30032e16d539e61d7a3b3b44c5f
LautaroPetaccio <lausuper@gmail.com>
2026-01-08 21:30:36 -0300

    feat: Garbage collection by scenes

--- test/integration/garbage-collection.spec.ts
+++ test/integration/garbage-collection.spec.ts
@@ -1,77 +1,195 @@
 import { test } from '../components'
 import { stringToUtf8Bytes } from 'eth-connect'
+import { Entity, IPFSv2 } from '@dcl/schemas'
 import { makeid } from '../utils'
 
-test('garbage collection works', function ({ components }) {
-  it('cleans up all unused files', async () => {
-    const { localFetch, storage, worldCreator } = components
+test('when performing garbage collection through /gc', function ({ components }) {
+  const { worldCreator, storage } = components
 
-    const worldName = worldCreator.randomWorldName()
+  describe('and there are unused files from a previous deployment', () => {
+    let worldName: string
+    let oldEntityId: IPFSv2
+    let oldEntity: Entity
+    let newEntityId: IPFSv2
+    let newEntity: Entity
 
-    // deploy an initial version of the scene
-    const files = new Map<string, Uint8Array>()
-    files.set('abc.png', stringToUtf8Bytes(makeid(150)))
-    files.set('abc.txt', stringToUtf8Bytes(makeid(50)))
+    beforeEach(async () => {
+      worldName = worldCreator.randomWorldName()
 
-    const { entityId, entity } = await worldCreator.createWorldWithScene({
-      worldName,
-      metadata: {
-        main: 'abc.txt',
-        scene: {
-          base: '20,24',
-          parcels: ['20,24']
+      // deploy an initial version of the scene
+      const files = new Map<string, Uint8Array>()
+      files.set('abc.png', stringToUtf8Bytes(makeid(150)))
+      files.set('abc.txt', stringToUtf8Bytes(makeid(50)))
+
+      const firstDeployment = await worldCreator.createWorldWithScene({
+        worldName,
+        metadata: {
+          main: 'abc.txt',
+          scene: {
+            base: '20,24',
+            parcels: ['20,24']
+          },
+          worldConfiguration: {
+            name: worldName
+          }
         },
-        worldConfiguration: {
-          name: worldName
-        }
-      },
-      files
-    })
+        files
+      })
 
-    expect(await storage.exist(entityId)).toBeTruthy()
-    expect(await storage.exist(`${entityId}.auth`)).toBeTruthy()
-    expect(await storage.exist(entity.content[0].hash)).toBeTruthy()
-    expect(await storage.exist(entity.content[1].hash)).toBeTruthy()
+      oldEntityId = firstDeployment.entityId
+      oldEntity = firstDeployment.entity
 
-    // deploy a new version of the scene
-    const newFiles = new Map<string, Uint8Array>()
-    newFiles.set('abc.png', stringToUtf8Bytes(makeid(150)))
-    newFiles.set('abc.txt', stringToUtf8Bytes(makeid(50)))
+      // deploy a new version of the scene
+      const newFiles = new Map<string, Uint8Array>()
+      newFiles.set('abc.png', stringToUtf8Bytes(makeid(150)))
+      newFiles.set('abc.txt', stringToUtf8Bytes(makeid(50)))
 
-    const { entityId: entityId2, entity: entity2 } = await worldCreator.createWorldWithScene({
-      worldName,
-      metadata: {
-        main: 'abc.txt',
-        scene: {
-          base: '20,24',
-          parcels: ['20,24']
+      const secondDeployment = await worldCreator.createWorldWithScene({
+        worldName,
+        metadata: {
+          main: 'abc.txt',
+          scene: {
+            base: '20,24',
+            parcels: ['20,24']
+          },
+          worldConfiguration: {
+            name: worldName
+          }
         },
-        worldConfiguration: {
-          name: worldName
+        files: newFiles
+      })
+
+      newEntityId = secondDeployment.entityId
+      newEntity = secondDeployment.entity
+    })
+
+    it('should respond with 200 and the count of removed keys', async () => {
+      const { localFetch } = components
+
+      // verify old files exist
+      expect(await storage.exist(oldEntityId)).toBeTruthy()
+      expect(await storage.exist(`${oldEntityId}.auth`)).toBeTruthy()
+      expect(await storage.exist(oldEntity.content![0].hash)).toBeTruthy()
+      expect(await storage.exist(oldEntity.content![1].hash)).toBeTruthy()
+
+      // verify new files exist
+      expect(await storage.exist(newEntityId)).toBeTruthy()
+      expect(await storage.exist(`${newEntityId}.auth`)).toBeTruthy()
+      expect(await storage.exist(newEntity.content![0].hash)).toBeTruthy()
+      expect(await storage.exist(newEntity.content![1].hash)).toBeTruthy()
+
+      const response = await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
         }
-      },
-      files: newFiles
+      })
+
+      expect(response.status).toEqual(200)
+      expect(await response.json()).toMatchObject({
+        message: 'Garbage collection removed 4 unused keys.'
+      })
     })
 
-    expect(await storage.exist(entityId2)).toBeTruthy()
-    expect(await storage.exist(`${entityId2}.auth`)).toBeTruthy()
-    expect(await storage.exist(entity2.content[0].hash)).toBeTruthy()
-    expect(await storage.exist(entity2.content[1].hash)).toBeTruthy()
+    it('should remove the old entity and auth files from storage', async () => {
+      const { localFetch, storage } = components
 
-    // run garbage collection
-    const response = await localFetch.fetch('/gc', {
-      method: 'POST',
-      headers: {
-        Authorization: 'Bearer setup_some_secret_here'
-      }
+      await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
+        }
+      })
+
+      expect(await storage.exist(oldEntityId)).toBeFalsy()
+      expect(await storage.exist(`${oldEntityId}.auth`)).toBeFalsy()
     })
 
-    // Check old files have been removed
-    expect(response.status).toEqual(200)
-    expect(await response.json()).toMatchObject({ message: 'Garbage collection removed 4 unused keys.' })
-    expect(await storage.exist(entityId)).toBeFalsy()
-    expect(await storage.exist(`${entityId}.auth`)).toBeFalsy()
-    expect(await storage.exist(entity.content[0].hash)).toBeFalsy()
-    expect(await storage.exist(entity.content[1].hash)).toBeFalsy()
+    it('should remove the old content files from storage', async () => {
+      const { localFetch, storage } = components
+
+      await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
+        }
+      })
+
+      expect(await storage.exist(oldEntity.content![0].hash)).toBeFalsy()
+      expect(await storage.exist(oldEntity.content![1].hash)).toBeFalsy()
+    })
+
+    it('should keep the active entity and auth files in storage', async () => {
+      const { localFetch, storage } = components
+
+      await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
+        }
+      })
+
+      expect(await storage.exist(newEntityId)).toBeTruthy()
+      expect(await storage.exist(`${newEntityId}.auth`)).toBeTruthy()
+    })
+
+    it('should keep the active content files in storage', async () => {
+      const { localFetch, storage } = components
+
+      await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
+        }
+      })
+
+      expect(await storage.exist(newEntity.content![0].hash)).toBeTruthy()
+      expect(await storage.exist(newEntity.content![1].hash)).toBeTruthy()
+    })
+  })
+
+  describe('and there are no unused files in storage', () => {
+    let worldName: string
+
+    beforeEach(async () => {
+      const { worldCreator } = components
+
+      worldName = worldCreator.randomWorldName()
+
+      // deploy only one version of the scene
+      const files = new Map<string, Uint8Array>()
+      files.set('abc.txt', stringToUtf8Bytes(makeid(50)))
+
+      await worldCreator.createWorldWithScene({
+        worldName,
+        metadata: {
+          main: 'abc.txt',
+          scene: {
+            base: '20,24',
+            parcels: ['20,24']
+          },
+          worldConfiguration: {
+            name: worldName
+          }
+        },
+        files
+      })
+    })
+
+    it('should respond with 200 and report no removed keys', async () => {
+      const { localFetch } = components
+
+      const response = await localFetch.fetch('/gc', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer setup_some_secret_here'
+        }
+      })
+
+      expect(response.status).toEqual(200)
+      expect(await response.json()).toMatchObject({
+        message: 'Garbage collection removed 0 unused keys.'
+      })
+    })
   })
 })
