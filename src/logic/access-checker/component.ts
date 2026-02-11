385fd7433a29aaf7885efaa6b91891ebfa7721a0
Kevin Szuchet <kevin.szuchet@decentraland.org>
2026-02-11 12:35:11 +0100

    refactor: Simplify access checker

--- src/logic/access-checker/component.ts
+++ src/logic/access-checker/component.ts
@@ -1,49 +1,10 @@
-import { AccessSetting, AccessType } from '../access/types'
+import { AccessSetting, AccessType, AllowListAccessSetting } from '../access/types'
 import { defaultAccess } from '../access/constants'
 import { EthAddress } from '@dcl/schemas'
 import bcrypt from 'bcrypt'
 import { ISocialServiceComponent } from '../../adapters/social-service'
 import { IWorldsManager } from '../../types'
 import { IAccessCheckerComponent } from './types'
-
-type CheckingFunction = (ethAddress: EthAddress, extras?: any) => Promise<boolean>
-
-function createUnrestrictedChecker(): CheckingFunction {
-  return (_ethAddress: EthAddress, _extras?: any): Promise<boolean> => {
-    return Promise.resolve(true)
-  }
-}
-
-function createSharedSecretChecker(hashedSharedSecret: string): CheckingFunction {
-  return (_ethAddress: EthAddress, plainTextSecret: string): Promise<boolean> => {
-    return bcrypt.compare(plainTextSecret, hashedSharedSecret)
-  }
-}
-
-function createNftOwnershipChecker(_requiredNft: string): CheckingFunction {
-  return (_ethAddress: EthAddress): Promise<boolean> => {
-    // TODO Check NFT ownership in the blockchain
-    return Promise.resolve(false)
-  }
-}
-
-function createAllowListCheckerFactory(socialService: ISocialServiceComponent) {
-  return (allowList: string[], communities: string[]): CheckingFunction => {
-    const lowerCasedAllowList = allowList?.map((ethAddress) => ethAddress.toLowerCase()) ?? []
-
-    return async (ethAddress: EthAddress, _extras?: any): Promise<boolean> => {
-      if (lowerCasedAllowList.includes(ethAddress.toLowerCase())) {
-        return true
-      }
-
-      if (communities && communities.length > 0) {
-        const { communities: memberCommunities } = await socialService.getMemberCommunities(ethAddress, communities)
-        return memberCommunities.length > 0
-      }
-      return false
-    }
-  }
-}
 
 export async function createAccessCheckerComponent({
   worldsManager,
@@ -52,21 +13,22 @@ export async function createAccessCheckerComponent({
   worldsManager: IWorldsManager
   socialService: ISocialServiceComponent
 }): Promise<IAccessCheckerComponent> {
-  const createAllowListChecker = createAllowListCheckerFactory(socialService)
+  async function checkAllowList(access: AllowListAccessSetting, ethAddress: EthAddress): Promise<boolean> {
+    const lowerCasedAllowList = access.wallets?.map((wallet) => wallet.toLowerCase()) ?? []
 
-  function createAccessCheckerFrom(accessSetting: AccessSetting): CheckingFunction {
-    switch (accessSetting.type) {
-      case AccessType.Unrestricted:
-        return createUnrestrictedChecker()
-      case AccessType.SharedSecret:
-        return createSharedSecretChecker(accessSetting.secret)
-      case AccessType.NFTOwnership:
-        return createNftOwnershipChecker(accessSetting.nft)
-      case AccessType.AllowList:
-        return createAllowListChecker(accessSetting.wallets, accessSetting.communities)
-      default:
-        throw new Error(`Invalid access type.`)
+    if (lowerCasedAllowList.includes(ethAddress.toLowerCase())) {
+      return true
     }
+
+    if (access.communities && access.communities.length > 0) {
+      const { communities: memberCommunities } = await socialService.getMemberCommunities(
+        ethAddress,
+        access.communities
+      )
+      return memberCommunities.length > 0
+    }
+
+    return false
   }
 
   async function getWorldAccess(worldName: string): Promise<AccessSetting> {
@@ -79,8 +41,20 @@ export async function createAccessCheckerComponent({
 
   async function checkAccess(worldName: string, ethAddress: EthAddress, extras?: any): Promise<boolean> {
     const access = await getWorldAccess(worldName)
-    const checker = createAccessCheckerFrom(access)
-    return checker(ethAddress, extras)
+
+    switch (access.type) {
+      case AccessType.Unrestricted:
+        return true
+      case AccessType.SharedSecret:
+        return bcrypt.compare(extras, access.secret) // extras being the secret provided by the user
+      case AccessType.NFTOwnership:
+        // TODO: Check NFT ownership in the blockchain
+        return false
+      case AccessType.AllowList:
+        return checkAllowList(access, ethAddress)
+      default:
+        throw new Error('Invalid access type.')
+    }
   }
 
   return { checkAccess, getWorldAccess }
