b2d296dab59a0ac7a06103a631f0d6e9cb06c47d
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2026-04-02 10:33:30 -0300

    feat: de-normalize world scenes (#475)

--- src/migrations/all-migrations.ts
+++ src/migrations/all-migrations.ts
@@ -22,6 +22,7 @@ import { migration as migration_0020 } from './0020_create_world_permissions_tab
 import { migration as migration_0021 } from './0021_migrate_permissions_to_world_permissions'
 import { migration as migration_0022 } from './0022_normalize_permission_parcels'
 import { migration as migration_0023 } from './0023_add_scene_deployment_status'
+import { migration as migration_0024 } from './0024_denormalize_world_scene_stats'
 
 export const allMigrations: Migration[] = [
   migration_0001,
@@ -46,5 +47,6 @@ export const allMigrations: Migration[] = [
   migration_0020,
   migration_0021,
   migration_0022,
-  migration_0023
+  migration_0023,
+  migration_0024
 ]
