8180afd82cc291b4e3e11fa805fa494c6af2e7b8
LautaroPetaccio <lausuper@gmail.com>
2026-08-05 18:28:14 -0300

    feat: filter world scenes by entity id

--- src/controllers/handlers/scenes-handler.ts
+++ src/controllers/handlers/scenes-handler.ts
@@ -1,7 +1,7 @@
 import { IHttpServerComponent } from '@dcl/core-commons'
 import { InvalidRequestError, NotAuthorizedError, getPaginationParams } from '@dcl/http-commons'
 import { DecentralandSignatureContext } from '@dcl/crypto-middleware'
-import { EthAddress } from '@dcl/schemas'
+import { EthAddress, IPFSv2 } from '@dcl/schemas'
 import { HandlerContextWithPath } from '../../types'
 import type { GetWorldScenesRequestBody } from '../schemas/scenes-query-schemas'
 import type { GetWorldScenesFilters } from '../../types'
@@ -51,14 +51,20 @@ export async function getScenesHandler(
 
   // Extract authorized_deployer filter
   const deployer = ctx.url.searchParams.get('authorized_deployer') ?? undefined
+  const entityId = ctx.url.searchParams.get('entity_id')
 
   // Validate authorized_deployer is a valid Ethereum address if provided
   if (deployer && !EthAddress.validate(deployer)) {
     throw new InvalidRequestError(`Invalid authorized_deployer address: ${deployer}. Must be a valid Ethereum address.`)
   }
 
+  if (entityId !== null && !IPFSv2.validate(entityId)) {
+    throw new InvalidRequestError(`Invalid entity_id: ${entityId}. Must be a valid IPFS CIDv1.`)
+  }
+
   const filters: GetWorldScenesFilters = {
     worldName: world_name,
+    ...(entityId !== null && { entityId }),
     ...(coordinates.length > 0 && { coordinates }),
     ...(boundingBox && { boundingBox }),
     ...(deployer && { authorized_deployer: deployer })
